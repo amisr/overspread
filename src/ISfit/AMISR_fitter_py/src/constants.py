4aaee02da74c4fc282edb36a4dc5fa713e9e59d6
Ashton Reimer <ashtonsethreimer@gmail.com>
2021-11-19 17:16:17 -0800

    formatting, use numpy.pi to be consistent

--- src/ISfit/AMISR_fitter_py/src/constants.py
+++ src/ISfit/AMISR_fitter_py/src/constants.py
@@ -8,15 +8,15 @@ last revised: xx/xx/2007
 
 """
 
-import math
+import numpy as np
 
-pi=math.pi
+pi = np.pi
 
 # some natural constants
-v_lightspeed=299792458
-v_Boltzmann=1.380658e-23
-v_electronmass=9.1093897e-31
-v_amu=1.6605402e-27
-v_electronradius=2.81794092e-15
-v_epsilon0=8.854188E-12
-v_elemcharge=1.602177E-19
+v_lightspeed = 299792458
+v_Boltzmann = 1.380658e-23
+v_electronmass = 9.1093897e-31
+v_amu = 1.6605402e-27
+v_electronradius = 2.81794092e-15
+v_epsilon0 = 8.854188e-12
+v_elemcharge = 1.602177e-19
