cab9bdcfb4efcd5c7b905ecf615e01db1d401e17
Ashton Reimer <ashtonsethreimer@gmail.com>
2021-11-24 17:07:50 -0800

    Updated shell scripts that help run the fitter

--- bin/run_LP.sh
+++ bin/run_LP.sh
@@ -3,25 +3,25 @@
 FIT_CONFIG='config_fit_LP.ini'
 IO_CONFIG='config_io_LP.ini'
 
-if [ $# -gt 3 ]; then
-    echo 1>&2 Usage: $0 EXPERIMENT [FIT_CONFIG] [IO_CONFIG]
+if [ $# -gt 4 ] || [ $# -eq 0 ]; then
+    echo 1>&2 Usage: $0 RADAR EXPERIMENT [FIT_CONFIG] [IO_CONFIG]
+    echo 1>&2        Valid RADAR: pfisr, risrn, risrc, sondre
     exit 127
 fi
 
-if [ $# -eq 2 ]; then
-    FIT_CONFIG=$2
-fi
 if [ $# -eq 3 ]; then
-    FIT_CONFIG=$2
-    IO_CONFIG=$3
+    FIT_CONFIG=$3
+fi
+if [ $# -eq 2 ]; then
+    FIT_CONFIG=$3
+    IO_CONFIG=$4
 fi
 
 # configuration files
-configsys="$AMISR_FITTER_PATH/config/config_sys.ini,$AMISR_FITTER_PATH/config/config_PFISR.ini" # system conf file
 configfiles="$FIT_CONFIG,$IO_CONFIG" # fit and io config files
-configexp="$1/config_exp.ini" # experiment specific config file(s)
+configexp="$2/config_exp.ini" # experiment specific config file(s)
 
 # run the fitter
-command="run_fitter -c $configsys,$configfiles,$configexp"
+command="run_fitter $1 -c $configfiles,$configexp"
 echo $command
 $command
